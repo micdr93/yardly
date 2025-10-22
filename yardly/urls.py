9bbf28172aef88024e14e2170206f95561dc55d7
Michael Drewe <michaeldrew_@outlook.com>
2025-10-22 23:54:47 +0100

    UPDATE: landing page and added navbar

--- yardly/urls.py
+++ yardly/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from django.shortcuts import render
 
-def home(request): return render(request, 'index.html')
+def home(request):
+    return render(request, 'pages/index.html')
 
-urlpatterns=[path('', home)]
+urlpatterns = [
+    path('', home, name='home'),
+]
